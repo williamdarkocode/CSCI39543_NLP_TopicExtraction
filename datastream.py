cbea22c0743a354c9be8968dad57c8cf58c4df09
William <williamd8323@afsenyc.org>
2022-05-21 11:27:53 -0400

    idk bruv

---
+++ datastream.py
@@ -0,0 +1,15 @@
+import requests
+import json_stream.requests
+
+def stream_from_url(data_uri, data_file):
+    
+    
+    return
+
+def main():
+    uri = "./dblpv13.json"
+    stream_from_url(uri, 'sample.json')
+
+
+if __name__ == '__main__':
+    main()
